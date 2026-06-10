9ea9e0e02db50990be1a4b6270fb3d2d8d51ef4b
EngrIBGIT <ibrahim5322022@hotmail.com>
2026-06-10 13:06:32 +0100

    Optimize APK size: shrinkResources, extractNativeLibs=false, aggro ProGuard, JS minification

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -1,4 +1,13 @@
+-optimizationpasses 5
+-overloadaggressively
+-repackageclasses ''
+-allowaccessmodification
+-mergeinterfacesaggressively
+
 -keep class com.facebook.hermes.** { *; }
 -keep class com.facebook.jni.** { *; }
 -keep class * implements com.facebook.react.bridge.NativeModule { *; }
+
 -dontwarn com.google.errorprone.annotations.**
+-dontwarn com.google.crypto.tink.**
+-dontwarn javax.annotation.**
--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    minifierConfig: {
+      mangle: { toplevel: true },
+      compress: { drop_console: true, drop_debugger: true, passes: 2 },
+      output: { ascii_only: true, comments: false },
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
